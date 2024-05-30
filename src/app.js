f201408299bb7d653e37ed281251c4ea0f06ec7e
blueclorox <dehydrate92@gmail.com>
2024-05-30 16:28:46 +0900

    해설 3강 기본세팅
    패키지 사용 준비 /  prettier 세팅 / status constant 입력

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 // src/app.js
 
 import express from 'express';
-import cookieParser from 'cookie-parser';
-import UsersRouter from './routers/users.router.js'
+import {SERVER_PORT} from './constants/env.constant.js'
+import { errorHandler } from './middlewares/error-handler.middleware.js';
+import { HTTP_STATUS } from './constants/http-status.constant.js';
 
 const app = express();
-const PORT = 3018;
 
 app.use(express.json());
-app.use(cookieParser());
-app.use('/api', [UsersRouter]);
+app.use(express.urlencoded({extended: true}));
 
-app.listen(PORT, () => {
-  console.log(PORT, `{PORT}포트로 서버가 열렸어요!`);
+app.get('/health-check', (req, res)=>{
+  return res.status(HTTP_STATUS.OK).send(`서버가 열려있습니다.`)
+})
+
+app.use(errorHandler);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`${SERVER_PORT}포트로 서버가 열렸어요!`);
 });
